8ad1f17abc1cb9af944ff9dd13f061271d4e8cc8
ralitong <ral.itong@relexsolutions.com>
2022-11-12 18:12:45 +0000

    - More progess for ^_^ - I am a bit sad today because of the things that - happened :( - But it's okay :D

--- chapter-16/andor.hs
+++ chapter-16/andor.hs
@@ -69,3 +69,110 @@ data Book = Book
 --     int yearPublished;
 --     double price;
 -- }
+
+-- Using a super class to get the similarities of Book and Vinyl Record
+-- public class StoreItem {
+--   String title;
+--   int yearPublished;
+--   double price;
+-- }
+
+-- public class Book extends StoreItem {
+--   Author author;
+--   String isbn;
+-- }
+
+-- public class VinylRecord extends StoreItem {
+--   String artist;
+-- }
+
+
+-- public class CollectibleToy {
+--   String name;
+--   String description;
+--   double price;
+-- }
+
+-- To include the CollectibleToy as a StoreItem (see above StoreItem class)
+-- You need to refactor StoreItem to only contain price property
+-- Because it's the only property that is common between CollectibleToy,
+-- Book and VinylRecord
+
+-- The ugly result ...
+-- public class StoreItem {
+--   double price;
+-- }
+
+-- public class Book extends StoreItem {
+--   Author author;
+--   String isbn;
+--   String title;
+--   int yearPublished;
+-- }
+
+-- public class VinylRecord extends StoreItem {
+--   String artist;
+--   String title;
+--   int yearPublished;
+-- }
+
+-- public class CollectibleToy {
+--   String name;
+--   String description;
+--   double price;
+-- }
+
+-- A Car type, SportsCar type and a Spoiler type
+-- data SportsCar = SportsCar Car Spoiler
+
+
+-- The sum type model enables you to define different sets
+-- of data
+
+-- A simple sum type model for Boolean, it can be either False or True
+data Bool = False | True
+
+-- A more complex sum type model for Name (FirstName & LastName)
+-- and NameWithMiddle (FirstName, MiddleName & LastName)
+type FirstName = String
+type LastName = String
+type MiddleName = String
+
+data Name = Name FirstName LastName 
+  | NameWithMiddle FirstName MiddleName LastName
+  -- Adding this data constructor for tricky edge-cases for Authors who uses initials
+  -- ex. H.P. Lovecraft or J.K. Rowling
+  | TwoInitialsWithLast Char Char LastName
+  -- Another example on how sum-type model can accomodate weird cases for an author who uses MiddleName and LastName initials
+  -- ex. Andrew W.K.
+  | FirstNameWithTwoInits FirstName Char Char
+
+-- Making a simpler way of implementing Artist and Author using sum-types
+data Creator = AuthorCreator Author | ArtistCreator Artist
+
+data Author = Author Name
+-- Using another sum type because an Artist can be either a Person or a Band
+data Artist = Person Name | Band String
+
+-- Here's how to create an instance of the Creator type
+
+-- Though this is a bit verbose, but its definietly better than
+-- hierarchical way (Superclass, Subclasses) of creating creators
+hpLoveCraft :: Creator
+hpLoveCraft = AuthorCreator(
+               (Author
+                (TwoInitialsWithLast 'H' 'P' "Lovecraft"))
+              )
+
+-- Accomodating weird cases like H.P. Lovecraft, Andrew W.K. in an OOP language
+-- would look like this
+
+-- public class Name {
+--   String firstName;
+--   String lastName;
+--   String middleName;
+--   char firstInitial;
+--   char middleInitial;
+--   char lastInitial;
+-- }
+
